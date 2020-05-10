1a679ba5f03e41ed92b91f0f6d66ac98b566bc02
Josh Soref <jsoref@users.noreply.github.com>
2020-05-10 15:22:32 -0400

    spelling: performed

--- plugins/inputs/snmp/snmp.go
+++ plugins/inputs/snmp/snmp.go
@@ -964,7 +964,7 @@ func SnmpTranslate(oid string) (mibName string, oidNum string, oidText string, c
 		// snmpTranslateCachesLock and instead wait on the individual
 		// snmpTranlsation.Lock to release. But I don't know that the extra complexity
 		// is worth it. Especially when it would slam the system pretty hard if lots
-		// of lookups are being perfomed.
+		// of lookups are being performed.
 
 		stc.mibName, stc.oidNum, stc.oidText, stc.conversion, stc.err = snmpTranslateCall(oid)
 		snmpTranslateCaches[oid] = stc
